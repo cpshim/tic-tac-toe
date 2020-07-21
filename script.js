4eac8aed55a524ee0b40654c6b05888b4a4fcd50
Chris <chris.shim1999@hotmail.com>
2020-07-21 19:17:52 -0400

    Implemented checkGameOver (brute force).

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const Gameboard = (() => {
     const isAvailable = (index) => 
         board[index] == "X" || board[index] == "O" ? false:true;
     const setPlayerTile = (index, sign) => board[index] = sign;
-    return {getBoard, isAvailable, setPlayerTile};
+    const clearBoard = () => board = [null, null, null, null, null, null, null, null, null];
+
+    return {getBoard, isAvailable, setPlayerTile, clearBoard};
 })();
 
 const Player = (sign) => {
@@ -31,7 +33,36 @@ const Game = (() => {
         return currentPlayer;
     }
 
-    return {swapCurrentPlayer, getCurrentPlayer};
+    function checkGameOver(){
+        let currentBoard = Gameboard.getBoard();
+        if ((currentBoard[0] === currentBoard[1]) && (currentBoard[0] === currentBoard[2]) && (currentBoard[0] != null)){
+            return true;
+        }
+        if ((currentBoard[3] === currentBoard[4]) && (currentBoard[3] === currentBoard[5]) && (currentBoard[3] != null)){
+            return true;
+        }
+        if ((currentBoard[6] === currentBoard[7]) && (currentBoard[6] === currentBoard[8]) && (currentBoard[6] != null)){
+            return true;
+        }
+        if ((currentBoard[0] === currentBoard[3]) && (currentBoard[0] === currentBoard[6]) && (currentBoard[0] != null)){
+            return true;
+        }
+        if ((currentBoard[1] === currentBoard[4]) && (currentBoard[1] === currentBoard[7]) && (currentBoard[1] != null)){
+            return true;
+        }
+        if ((currentBoard[2] === currentBoard[5]) && (currentBoard[2] === currentBoard[8]) && (currentBoard[2] != null)){
+            return true;
+        }
+        if ((currentBoard[0] === currentBoard[4]) && (currentBoard[0] === currentBoard[8]) && (currentBoard[0] != null)){
+            return true;
+        }
+        if ((currentBoard[2] === currentBoard[4]) && (currentBoard[2] === currentBoard[6]) && (currentBoard[2] != null)){
+            return true;
+        }
+        return false;
+    }
+
+    return {swapCurrentPlayer, getCurrentPlayer, checkGameOver};
 })();
 
 function render(){
@@ -50,6 +81,7 @@ function render(){
                     Game.getCurrentPlayer().sign);
                 Game.swapCurrentPlayer();
             }
+            console.log(Game.checkGameOver());
             render();
         });
 
